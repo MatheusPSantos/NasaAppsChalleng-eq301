94022f3be0e7c8235f70849b1247e6c7cb3abc02
matheuspsantos <msantos.2015@alunos.utfpr.edu.br>
2020-05-31 01:29:55 -0300

    initialize chat

---
+++ app/Controllers/Ws/ChatController.js
@@ -0,0 +1,14 @@
+'use strict'
+
+class ChatController {
+  constructor({ socket, request }) {
+    this.socket = socket
+    this.request = request
+  }
+
+  onMessage(data) {
+    this.broadcastToAll('message', data)
+  }
+}
+
+module.exports = ChatController
--- start/app.js
+++ start/app.js
@@ -17,6 +17,7 @@ const providers = [
   '@adonisjs/cors/providers/CorsProvider',
   '@adonisjs/lucid/providers/LucidProvider',
   'adonis-mongoose-model/providers/MongooseProvider',
+  '@adonisjs/websocket/providers/WsProvider'
 ]
 
 /*
